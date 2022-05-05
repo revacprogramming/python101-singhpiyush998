3781484af267e2dbeb16315f97c1d45cfb88bfef
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-05 17:06:26 +0530

    problem 14 done , Added reg_ex.text in datasets

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,15 @@
-# Using Web Services
-# https://www.py4e.com/lessons/servces
+#The basic outline of this problem is to read the file, look for integers using the re.findall(), 
+# looking for a regular expression of '[0-9]+' 
+# and then converting the extracted strings to integers and summing up the integers.
+import re
+
+fh = open("dataset/reg_ex.txt")
+sum = 0
+
+for line in fh:
+    numbers = re.findall('[0-9]+', line)
+    if len(numbers) > 0:
+        for number in numbers:
+            sum += int(number)
+
+print(sum)
