da790caf13a7a344901c2e02bd8c4dabfd329e3e
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-05 15:42:16 +0530

    7th done

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,19 @@
-# Strings
+# 5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers. If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below.
 
-text = "X-DSPAM-Confidence:    0.8475"
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num  = int(num)
+        if largest is None or num > largest:
+            largest = num
+        elif smallest is None or num < smallest:
+            smallest = num
+    except:
+        print("Invalid input")
+
+print(f"Maximum is {largest}")
+print(f"Minimum is {smallest}")
