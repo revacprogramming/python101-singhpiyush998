192eb636e388524bcb1c81c96e2dbfe95dc33de5
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-07 09:13:02 +0530

    15 done

--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,2 +1,22 @@
-# Object Oriented Programming
-# https://www.py4e.com/lessons/Objects
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+
+sum = 0
+# Retrieve all of the span tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    sum += int(tag.contents[0])
+
+print(sum)
