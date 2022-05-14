9428fa80e7a5002d1891d4737319cd190c1a9cfa
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-14 14:00:19 +0530

    REDO: problem03

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,6 +1,25 @@
 # This first line is provided for you
 
-hrs = float(input("Enter Hours:"))
-rate = float(input("Enter rate:"))
 
-print("Pay: " + str(hrs * rate))
+
+# print("Pay: " + str(hrs * rate))
+
+
+def my_input():
+    h = float(input("Enter Hours:"))
+    r = float(input("Enter rate:"))
+    return h, r
+
+def calc_pay(h, r):
+    return h * r
+
+def output(p):
+    print("Pay:", p)
+
+def main():
+    hrs, rate = my_input()
+    pay = calc_pay(hrs, rate)
+    output(pay)
+
+if __name__ == "__main__":
+    main()
