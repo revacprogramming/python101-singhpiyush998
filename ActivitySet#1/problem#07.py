c0e2ec0b077f90d77042f2f875006532366f6edd
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-14 14:46:13 +0530

    REDO: Problem#07

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,19 +1,34 @@
 # 5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers. If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below.
 
-largest = None
-smallest = None
-while True:
-    num = input("Enter a number: ")
-    if num == "done":
-        break
-    try:
-        num  = int(num)
-        if largest is None or num > largest:
-            largest = num
-        elif smallest is None or num < smallest:
-            smallest = num
-    except:
-        print("Invalid input")
+def my_input():
+    n = input("Enter a number: ")
+    return n
+    
+def find_lowest_and_smallest(n, l, s):
+        try:
+            n  = int(n)
+            if l is None or n > l:
+                l = n
+            elif s is None or n < s:
+                s = n
+        except:
+            print("Invalid input")
+        return l, s
 
-print(f"Maximum is {largest}")
-print(f"Minimum is {smallest}")
+def output(l, s):
+    print("Maximum is", l)
+    print("Maximum is", s)
+
+def main():
+    largest = None
+    smallest = None
+    while True:
+        num = my_input()
+        if num == "done":
+            break
+        largest, smallest = find_lowest_and_smallest(num, largest, smallest)
+    
+    output(largest, smallest)
+
+if __name__ == "__main__":
+    main()
