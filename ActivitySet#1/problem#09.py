b493b4ff9f91f4304136f04144d9ef952cc00ec6
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-18 16:59:52 +0530

    REDO: Problem 9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -4,13 +4,28 @@ Count these lines and extract the floating point values from each of the lines a
 You can download the sample data at http://www.py4e.com/code3/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
 """
 
-fname = input("Enter file name: ")
-total = 0
-count = 0
-fh = open(fname)
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:"):
-        continue
-    total += float(line[line.find('0'):])
-    count += 1
-print(f"Average spam confidence: {total / count}")
+def my_input():
+    fname = input("Enter file name: ")
+    fh = open(fname)
+    return fh
+
+def find_avg(fh):
+    total = 0
+    count = 0
+    for line in fh:
+        if not line.startswith("X-DSPAM-Confidence:"):
+            continue
+        total += float(line[line.find('0'):])
+        count += 1
+    return total / count
+
+def output(avg):
+    print("Average spam confidence:", avg)
+
+def main():
+    fh = my_input()
+    avg = find_avg(fh)
+    output(avg)
+
+if __name__ == "__main__":
+    main()
