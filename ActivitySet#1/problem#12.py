4dfd801d0a5a2a328b1df1e92922ea88e6a0bf94
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-18 17:23:19 +0530

    REDO: Problem12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,23 +1,40 @@
-name = input("Enter file:")
-if len(name) < 1:
-    name = "../dataset/mbox-short.txt"
-handle = open(name)
+from gettext import find
 
-email_count = dict()
 
-for line in handle:
-    if line.startswith("From") and not line.startswith("From:"):
-        words = line.split()
-        if words[1] in email_count.keys():
-            email_count[words[1]] = email_count[words[1]] + 1
-        else:
-            email_count[words[1]] = 1
+def my_input():
+    name = input("Enter file:")
+    if len(name) < 1:
+        name = "dataset/mbox-short.txt"
+    fh = open(name)
+    return fh
 
-max_count = 0
-max_count_email = ""
-for k,v in email_count.items():
-    if v > max_count:
-        max_count = v
-        max_count_email = k
+def find_max_count_email(fh):
+    email_count = dict()
 
-print(max_count_email, max_count)
+    for line in fh:
+        if line.startswith("From") and not line.startswith("From:"):
+            words = line.split()
+            if words[1] in email_count.keys():
+                email_count[words[1]] += 1
+            else:
+                email_count[words[1]] = 1
+
+    max_count = 0
+    max_count_email = ""
+    for k,v in email_count.items():
+        if v > max_count:
+            max_count = v
+            max_count_email = k
+
+    return max_count, max_count_email
+
+def output(max_count, max_count_email):
+    print(max_count_email, max_count)
+
+def main():
+    fh = my_input()
+    max_count, max_count_email = find_max_count_email(fh)
+    output(max_count, max_count_email)
+
+if __name__ == "__main__":
+    main()
