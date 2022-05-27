2baee8581d8989ffa9507bf57c5b1b021de0f8df
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-27 15:55:18 +0530

    REDO: Problem 14

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -2,14 +2,28 @@
 # looking for a regular expression of '[0-9]+' 
 # and then converting the extracted strings to integers and summing up the integers.
 import re
+def my_input():
+    fh = open("../dataset/reg_ex.txt")
+    return fh
 
-fh = open("dataset/reg_ex.txt")
-sum = 0
+def find_sum(fh):
+    sum = 0
 
-for line in fh:
-    numbers = re.findall('[0-9]+', line)
-    if len(numbers) > 0:
-        for number in numbers:
-            sum += int(number)
+    for line in fh:
+        numbers = re.findall('[0-9]+', line)
+        if len(numbers) > 0:
+            for number in numbers:
+                sum += int(number)
 
-print(sum)
+    return sum
+
+def output(sum):
+    print(sum)
+
+def main():
+    fh = my_input()
+    sum = find_sum(fh)
+    output(sum)
+
+if __name__ == "__main__":
+    main()
