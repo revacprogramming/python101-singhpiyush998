1e6f41e6e61c832d849b05a5e1e2f8bb8d156cf8
singhpiyush998 <singhpiyush998@gmail.com>
2022-06-03 14:41:20 +0530

    Complete: Problem1

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,15 @@
 
 
 def add(a, b):
-    return  # ...
+    return  int(a) + int(b)
 
 
 def main():
-    a = input  # ...
-    b = input  # ...
+    a = input("Enter 1st Number? ")
+    b = input("Enter 2nd Number? ")
 
     c = add(a, b)
-    print  # ...
+    print(f"sum of {a} and {b} is {c}")
+
+if __name__ == "__main__":
+    main()
