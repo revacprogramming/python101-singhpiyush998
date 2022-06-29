7aba64cd6612d73bd929357cd722c004059f977b
singhpiyush998 <singhpiyush998@gmail.com>
2022-06-29 23:19:50 +0530

    Do: Problem 5

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -6,16 +6,18 @@ def get_cs():
 
 def cs_to_dict(cs):
     """convert connect string to a dictionary"""
+    return dict(i.split('=') for i in cs.split(';'))
 
 
 def dict_to_cs(d):
     """convert a dictionary to connect string"""
+    return ';'.join('='.join(i) for i in d.items())
 
 
 def main():
     cs = get_cs()
 
-    d = cs_to_dict(cs) # convert connect string to a dictionary
+    d = cs_to_dict(cs)  # convert connect string to a dictionary
     print(d)
 
     cs = dict_to_cs(d)
