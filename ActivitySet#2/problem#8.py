f0686dce6030fb8b8ac8c6088b3ab7a01c183985
singhpiyush998 <singhpiyush998@gmail.com>
2022-06-30 21:27:58 +0530

    Complete: Problem 8

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -2,8 +2,15 @@
 
 class Menu:
     """fill in class definition here"""
+    def __init__(self):
+        self.orders = dict()
 
-
+    def __setitem__(self, k, v):
+        self.orders[k] = v
+        
+    def __str__(self):
+        return "\n".join(" ".join((k, str(v))) for k, v in self.orders.items())
+    
 m = Menu()
 m["idly"] = 10
 m["vada"] = 20
